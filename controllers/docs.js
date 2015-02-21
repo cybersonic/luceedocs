c50414f1691f6e6e023d5d2b43a82ab2461e788f
Jon Clausen <jon_clausen@silowebworks.com>
2015-02-20 20:19:50 -0500

    add 404 handling to docs

--- controllers/docs.js
+++ controllers/docs.js
@@ -4,11 +4,17 @@ var marked = require('marked');
 
 
 exports.get  = function(req, res){
-
-
-    var md = fs.readFileSync(path.resolve("./docs/" + req.params.page +".md"), 'utf-8');
-    //if we find it right?
-    res.render('docs/display', {content:marked.parse(md),page: req.params.page +".md"});
+	//check if exists, otherwise 404
+	fs.exists(path.resolve("./docs/" + req.params.page +".md"),function(exists){
+		if(exists){
+			var md = fs.readFileSync(path.resolve("./docs/" + req.params.page +".md"), 'utf-8');
+    		res.render('docs/display', {content:marked.parse(md),page: req.params.page +".md"});	
+		} else {
+			return res.render('404', { status: 404, url: req.url });	
+		}
+	});
+	
+    
 }
 
 
